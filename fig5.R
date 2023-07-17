08f705abdcdf79e324304681ee1957608e25b293
Hyu-Kim <hskim0000@gmail.com>
2023-07-17 16:46:26 -0400

    fig5a updated

--- fig5.R
+++ fig5.R
@@ -1,6 +1,7 @@
 ## Combines count and Cnet data in one plot (day 14)
 library("dplyr")
 library(ggplot2)
+library(scales)
 
 # import data
 count_bact <- read.csv("data/flow2_day14_bact.csv")
@@ -30,19 +31,34 @@ for (row in 1:nrow(df)){
 
 # plot
 setEPS()
-postscript("figures/fig5a.eps", width = 3, height = 3)
+postscript("figures/fig5a.eps", width = 2, height = 2)
 
 ggplot(df, aes(x=cnet_q50, y=count_mean, shape=ring, colour=treatment)) + 
-  geom_point() + 
-  geom_errorbar(aes(ymax = count_mean+count_sd, ymin = count_mean-count_sd), width=0.02) + 
-  geom_errorbarh(aes(xmax = cnet_q75, xmin = cnet_q25), height=0.02) + 
+  geom_point(size=2.5, stroke=0.5) + 
+  geom_errorbar(aes(ymax = count_mean+count_sd, ymin = count_mean-count_sd), width=0.02, linewidth=0.2) + 
+  geom_errorbarh(aes(xmax = cnet_q75, xmin = cnet_q25), height=0.02, linewidth=0.2) + 
+  # scale_x_log10(breaks = trans_breaks("log10", function(x) 10^x),
+  #               labels = trans_format("log10", math_format(10^.x))) +
   scale_x_continuous(trans='log10') +
   scale_y_continuous(trans='log10') +
-  scale_color_manual(values=c("#C00000", "#0432FF", "#AB7942", "#000000")) +
-  scale_shape_manual(values = c(16,1)) +
+  annotation_logticks(short = unit(0.1, "cm"),
+                      mid = unit(0.1, "cm"),
+                      long = unit(0.2, "cm"),
+                      size = 0.3) +
+  scale_color_manual(values=c("#C00000", "#0432FF", "#AB7942", "#000000")) +  # Alcani, Devosi, Marino, none
+  scale_shape_manual(values = c(16,1)) +  # Inner, outer
   theme(strip.background = element_rect(fill=NA),
         panel.background = element_rect(fill = "transparent", color = NA),
-        # panel.grid.major = element_blank(),
-        # panel.grid.minor = element_blank(),
+        panel.grid.major = element_line(colour = "grey80", linewidth=0.1),
+        # panel.grid.minor = element_line(colour = "grey80", linewidth=0.2),
         plot.background = element_rect(fill = "transparent", color = NA),
-        panel.border = element_rect(colour = "black", fill=NA, size=0.5))
+        panel.border = element_blank(),
+        legend.position = "None",
+        axis.title.x = element_blank(),
+        axis.title.y = element_blank(),
+        axis.text.x = element_blank(),
+        axis.text.y = element_blank(),
+        axis.line = element_line(size = 0.2),
+        )
+
+dev.off()
