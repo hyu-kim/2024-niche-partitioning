6a322340a47ab9eaeeca6983111a521ba7829b33
Hyu-Kim <hskim0000@gmail.com>
2023-07-18 15:48:28 -0400

    fig 5b, no labels text

--- fig5b.R
+++ fig5b.R
@@ -10,6 +10,9 @@ cnet <- read.csv("data/SIP_cnet.csv")
 cnet_info <- read.csv("data/SIP_sample_info.csv")
 biovolume <- read.table("data/biovolume.txt", header = TRUE, sep = ',')
 
+count_bact$Ring[count_bact$Ring==1] <- 'inner'
+count_bact$Ring[count_bact$Ring==2] <- 'outer'
+
 # Here we assume that bacterial cell number and Cnet are conditionally 
 # independent given algal cell number. For each microplate, the amount of 
 # bacterial incorporation of algal carbon is estimated by multiplying Cnet median
@@ -17,49 +20,90 @@ biovolume <- read.table("data/biovolume.txt", header = TRUE, sep = ',')
 # to disentangle the algal effect. Normalized values are used for statistical
 # analysis, e.g., average or sd.
 
-# get statistics from count data
-count_bact_stat <- count_bact %>%
-  group_by(Treatment, Ring) %>%
-  summarize(mean = mean(Abundance), 
-            sd = sd(Abundance))
+# summarize count data
+count_bact_summ <- count_bact %>%
+  group_by(Treatment, Ring, Microplate) %>%
+  summarize(abd_mean = mean(Abundance), 
+            abd_sd = sd(Abundance),
+            count_total = 6*mean(Abundance)
+            ) # merge over 6 wells per ring
 
-# merbe count and cnet in to df
-count_bact_stat$Ring[count_bact_stat$Ring==1] <- 'inner'
-count_bact_stat$Ring[count_bact_stat$Ring==2] <- 'outer'
-df <- cnet_stat
-colnames(df) <- c("treatment", "ring", "cnet_q25", "cnet_q50", "cnet_q75")
-df$count_mean <- NA
-df$count_sd <- NA
+# summarize cnet
+cnet$microplate <- NA
+cnet$ring <- NA
+cnet$treatment <- NA
+cnet$strain <- NA
+for (s in unique(cnet_info$sample_name)){
+  m <- cnet_info$microplate[cnet_info$sample_name==s]
+  r <- cnet_info$ring[cnet_info$sample_name==s]
+  t <- cnet_info$treatment[cnet_info$sample_name==s]
+  st <- cnet_info$strain[cnet_info$sample_name==s]
+  
+  cnet$microplate[cnet$sample_name==s] <- m
+  cnet$ring[cnet$sample_name==s] <- r
+  cnet$treatment[cnet$sample_name==s] <- t
+  cnet$strain[cnet$sample_name==s] <- st
+}
+
+cnet_summ <- cnet %>%
+  group_by(treatment, microplate, ring, strain) %>%
+  summarize(cnet_q50 = quantile(Cnet, probs = 0.5), 
+            cnet_q25 = quantile(Cnet, probs = 0.25),
+            cnet_q75 = quantile(Cnet, probs = 0.75)
+            )
+
+# merge count, cnet, biovolume, algal count in to df
+df <- select(cnet_summ, treatment, microplate, ring, strain, cnet_q50)
+df$count_total <- NA
+df$biovolume <- NA
+df$count_pt <- NA
+df <- df[df$strain!='none',]
 
 for (row in 1:nrow(df)){
   t <- df$treatment[row]
+  m <- df$microplate[row]
   r <- df$ring[row]
-  df[row, 'count_mean'] <- count_bact_stat$mean[count_bact_stat$Treatment==t & count_bact_stat$Ring==r]
-  df[row, 'count_sd'] <- count_bact_stat$sd[count_bact_stat$Treatment==t & count_bact_stat$Ring==r]
+  st <- df$strain[row]
+  df[row, 'count_total'] <- count_bact_summ$count_total[
+    count_bact_summ$Treatment == t & 
+      count_bact_summ$Ring == r &
+      count_bact_summ$Microplate == m
+    ]
+  df[row, 'biovolume'] <- biovolume$Biovolume[biovolume$Genus==st]
+  df[row, 'count_pt'] <- count_pt$Abundance[
+    count_pt$Treatment==t & 
+      count_pt$Microplate==m
+    ]
 }
 
-# plot
-setEPS()
-postscript("figures/fig5b.eps", width = 2.5, height = 2)
+df$c_incorp <- df$cnet_q50 * df$count_total * df$biovolume
+df$c_incorp_pt <- df$c_incorp / df$count_pt
 
-ggplot(df, aes(x=cnet_q50, y=count_mean, shape=ring, colour=treatment)) + 
-  geom_point(size=2.5, stroke=0.5) + 
-  geom_errorbar(aes(ymax = count_mean+count_sd, ymin = count_mean-count_sd), width=0.02, linewidth=0.2) + 
-  geom_errorbarh(aes(xmax = cnet_q75, xmin = cnet_q25), height=0.02, linewidth=0.2) + 
-  # scale_x_log10(breaks = trans_breaks("log10", function(x) 10^x),
-  #               labels = trans_format("log10", math_format(10^.x))) +
-  scale_x_continuous(trans='log10', breaks=c(0.1)) +
-  scale_y_continuous(trans='log10', breaks=c(10^6, 10^7)) +
-  annotation_logticks(short = unit(0.1, "cm"),
-                      mid = unit(0.1, "cm"),
-                      long = unit(0.2, "cm"),
-                      size = 0.3) +
-  scale_color_manual(values=c("#C00000", "#0432FF", "#AB7942", "#000000")) +  # Alcani, Devosi, Marino, none
-  scale_shape_manual(values = c(16,1)) +  # Inner, outer
+# remove Alcani, microplate 2 from df, confirming with XM
+df <- df[!(df$treatment=='Alcanivorax' & df$microplate==2),]
+
+# plot
+pdf("figures/fig5b.pdf", width = 2.5, height = 2)
+
+ggplot(df, aes(x=microplate, 
+               y=c_incorp_pt, 
+               fill=treatment, 
+               alpha=factor(ring, levels=c("outer","inner"))
+               )
+       ) + 
+  geom_bar(stat = 'identity', position = 'stack', color='black', size=0.2) +
+  facet_grid(~factor(treatment, 
+                     levels=c('Devosia', 'Marinobacter', 'Alcanivorax', 'none')
+                     )) +
+  # geom_text(aes(label=round(c_incorp_pt, digits = 4)), vjust=0.5, alpha=1) +
+  ylim(0, 0.08) +
+  # ylab("Incorporated algal carbon (um3)") +
+  scale_fill_manual(values=c("#E06666", "#4061f4", "#AB7942", "#5b5b5b")) +  # Alcani, Devosi, Marino, none
+  scale_alpha_discrete(range=c(0.3, 1)) +
   theme(strip.background = element_rect(fill=NA),
         panel.background = element_rect(fill = "transparent", color = NA),
-        panel.grid.major = element_line(colour = "grey80", linewidth=0.1),
-        # panel.grid.minor = element_line(colour = "grey80", linewidth=0.2),
+        panel.grid.major = element_blank(),
+        panel.grid.minor = element_blank(),
         plot.background = element_rect(fill = "transparent", color = NA),
         panel.border = element_blank(),
         legend.position = "None",
@@ -68,7 +112,8 @@ ggplot(df, aes(x=cnet_q50, y=count_mean, shape=ring, colour=treatment)) +
         axis.text.x = element_blank(),
         axis.text.y = element_blank(),
         axis.line = element_line(size = 0.2),
-        axis.ticks = element_blank()
+        strip.text.x = element_blank()
+        # axis.ticks = element_blank()
         )
 
 dev.off()
