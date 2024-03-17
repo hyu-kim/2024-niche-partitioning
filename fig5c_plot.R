150f63841e2e37251e105fc6bff35bdbab20f4ee
Hyu-Kim <hskim0000@gmail.com>
2024-03-16 21:35:46 -0400

    files renamed

--- fig5c_plot.R
+++ fig5c_plot.R
@@ -1,29 +1,30 @@
-source('fig5c_probmodel.R')
+source("fig5b.R")
 
-total_incorp_ratio <- get_ratio_df()
+df_summ <- get_total_incorp_df()$df_summ
 
-# plot results
-
+# plot, average over microplates
 setEPS()
-postscript("figures/fig5c.eps", width = 1.7, height = 1.7)
+postscript("figures/fig5c_v2.eps", width = 1.35, height = 2.3)
 
-ggplot(total_incorp_ratio, aes(x=pred_mean, y=incorp_ratio, colour=Treatment)) + 
-
-  geom_point(size=2, stroke=0.5) + 
-  geom_errorbarh(aes(xmax = pred_mean+pred_sd, xmin = pred_mean-pred_sd), height=0.02, linewidth=0.2) + 
-  # scale_x_log10(breaks = trans_breaks("log10", function(x) 10^x),
-  #               labels = trans_format("log10", math_format(10^.x))) +
-  scale_x_continuous(trans='log10') +
-  scale_y_continuous(trans='log10') +
-  annotation_logticks(short = unit(0.1, "cm"),
-                      mid = unit(0.1, "cm"),
-                      long = unit(0.2, "cm"),
-                      size = 0.15) +
-  scale_color_manual(values=c("#C00000", "#0432FF", "#AB7942", "#000000")) +  # Alcani, Devosi, Marino, none
+df_summ %>% mutate(SD = c_incorp_sd) %>%
+  group_by(treatment) %>%
+  mutate(SDPos = cumsum(c_incorp_mean)) %>%
+  ggplot(aes(x = reorder(treatment, -c_incorp_mean), y = c_incorp_mean, fill = treatment
+             # alpha=factor(ring, levels=c("outer","inner"))
+             )) +
+  geom_bar(stat = "identity", color='black', size=0.2, width=0.7) +
+  # geom_text(aes(label = paste(round(c_incorp_mean,3), '±', round(c_incorp_sd,3))), 
+  #           position = position_stack(vjust =  0.5)) +
+  geom_errorbar(aes(ymin = SDPos-SD, ymax = SDPos+SD), width=0.1, linewidth=0.2, position = "identity", color='black') +
+  scale_alpha_discrete(range=c(0.3, 1)) +
+  # scale_fill_manual(values=c("#FFDFE1", "#D2DCFB", "#F8DFC4", "#D1D3D4")) +
+  scale_fill_manual(values=c("#BC2329", "#4059A7", "#AB7942", "white")) +  
+  # Alcani, Devosi, Marino, none
+  ylab('carbon mass incorporation rate (ng C d-1)') +
   theme(strip.background = element_rect(fill=NA),
         panel.background = element_rect(fill = "transparent", color = NA),
         panel.grid.major = element_blank(),
-        # panel.grid.minor = element_line(colour = "grey80", linewidth=0.2),
+        panel.grid.minor = element_blank(),
         plot.background = element_rect(fill = "transparent", color = NA),
         panel.border = element_blank(),
         legend.position = "None",
@@ -31,8 +32,46 @@ ggplot(total_incorp_ratio, aes(x=pred_mean, y=incorp_ratio, colour=Treatment)) +
         axis.title.y = element_blank(),
         axis.text.x = element_blank(),
         axis.text.y = element_blank(),
-        axis.line = element_line(size = 0.2),
-        axis.ticks = element_line(size = 0.2)
+        axis.line = element_line(size = 0.2)
+        # axis.ticks = element_blank()
   )
 
 dev.off()
+
+
+
+## By normalizing with Pt cell number (depreciated)
+# df$c_incorp <- df$cnet_q50 * df$count_total * df$mass_c
+# df$c_incorp_pt <- df$c_incorp / (df$count_pt * mass[4, 'mass'])
+# 
+# pdf("figures/fig5b.pdf", width = 2.5, height = 2)
+# 
+# ggplot(df, aes(x=microplate, 
+#                y=c_incorp_pt, 
+#                fill=treatment, 
+#                alpha=factor(ring, levels=c("outer","inner"))
+#                )
+#        ) + 
+#   geom_bar(stat = 'identity', position = 'stack', color='black', size=0.2) +
+#   facet_grid(~factor(treatment, 
+#                      levels=c('Devosia', 'Marinobacter', 'Alcanivorax', 'none')
+#                      )) +
+#   scale_fill_manual(values=c("#E06666", "#4061f4", "#AB7942", "#5b5b5b")) +  # Alcani, Devosi, Marino, none
+#   scale_alpha_discrete(range=c(0.3, 1)) +
+#   theme(strip.background = element_rect(fill=NA),
+#         panel.background = element_rect(fill = "transparent", color = NA),
+#         panel.grid.major = element_blank(),
+#         panel.grid.minor = element_blank(),
+#         plot.background = element_rect(fill = "transparent", color = NA),
+#         panel.border = element_blank(),
+#         legend.position = "None",
+#         axis.title.x = element_blank(),
+#         axis.title.y = element_blank(),
+#         axis.text.x = element_blank(),
+#         axis.text.y = element_blank(),
+#         axis.line = element_line(size = 0.2),
+#         strip.text.x = element_blank()
+#         # axis.ticks = element_blank()
+#         )
+# 
+# dev.off()
